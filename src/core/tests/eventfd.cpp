569d30ba6cb92d7d4e53af99bc7deb0b58a4aebd
Robert Leahy <rleahy@rleahy.ca>
2019-05-12 19:20:19 +0000

    uring tests
    - Removed broken reading test for asio_uring::eventfd_queue
    - Added asio_uring::uring tests for asio_uring::eventfd
    
    Note: As best I can tell issuing asynchronous reads against an eventfd
    file descriptor using a ring always completes immediately with error
    code -22 (i.e. EINVAL). This suggests to me that this is not supported
    and that the less efficient (due to more syscalls) poll then read
    approach will have to be taken.

--- src/core/tests/eventfd.cpp
+++ src/core/tests/eventfd.cpp
@@ -3,8 +3,12 @@
 #include <limits>
 #include <optional>
 #include <system_error>
+#include <asio_uring/liburing.hpp>
+#include <asio_uring/uring.hpp>
 #include <errno.h>
+#include <poll.h>
 #include <sys/eventfd.h>
+#include <sys/uio.h>
 
 #include <catch2/catch.hpp>
 
@@ -80,6 +84,34 @@ TEST_CASE("eventfd semaphore",
   CHECK(result == 1);
   result = e.read();
   CHECK(result == 1);
+}
+
+TEST_CASE("eventfd & uring poll",
+          "[eventfd][uring]")
+{
+  uring u(1);
+  eventfd e;
+  ::io_uring_sqe* sqe = ::io_uring_get_sqe(u.native_handle());
+  REQUIRE(sqe);
+  ::io_uring_prep_poll_add(sqe,
+                           e.native_handle(),
+                           POLLIN);
+  sqe->user_data = 5;
+  REQUIRE_FALSE(::io_uring_get_sqe(u.native_handle()));
+  ::io_uring_submit(u.native_handle());
+  ::io_uring_cqe* cqe;
+  int result = ::io_uring_peek_cqe(u.native_handle(),
+                                   &cqe);
+  REQUIRE(result == 0);
+  REQUIRE_FALSE(cqe);
+  e.write(1);
+  result = ::io_uring_peek_cqe(u.native_handle(),
+                               &cqe);
+  REQUIRE(result == 0);
+  REQUIRE(cqe);
+  CHECK(cqe->user_data == 5);
+  REQUIRE(cqe->res >= 0);
+  CHECK(e.read() == 1);
 }
 
 }
